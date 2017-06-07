16eb1dc4c1664db3faf513fd12a52a7e60540dd6
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-07 15:32:07 -0500

    finishing inclass work

--- objects.js
+++ objects.js
@@ -1,36 +1,64 @@
-const song = {
-	title: "Private Idaho",
-	artist: "The B-52s",
-	plays: 120,
-	album: "Wild Planet",
-	bandMembers: [
-		{name: "Fred", roles: {vocals: "lead", instrument: null} },
-		{name: "Cindy", roles: {vocals: "lead", instrument: "keyboard"} },
-		{name: "Ricky", roles: {vocals: "background", instrument: "guitar"} }, 
-		{name: "Keith", roles: {vocals: "null", instrument: "drums"} },
-		{name: "Kate", roles: {vocals: "lead", instrument: null} }
-	],
+const songMaker = {
 	makeSongString: function() {
 		return `"${song.title}" is from the album "${song.album}" by ${song.artist}`
-	} 
-
-}
-//var bandMembers
-
-const member = song.bandMembers;
-
-for (var i = 0; i < member.length; i++) {
-	if ("lead" === member[i].roles.vocals) { 
-		document.write(`"${member[i].name} " is a lead singer on " ${song.album}"`);
-		console.log("blah",`"${member[i].name} " is a lead singer on " ${song.album}"`);
-	} else if ("background" === member[i].roles.vocals) {
-		document.write(`"${member[i].name}" is a background singer on "${song.album}"`);
-		console.log("ha",`"${member[i].name} " is a lead singer on " ${song.album}"`);
-	} else {
-		document.write(`"${member[i].name}" did not sing on "${song.album}"`);
-		console.log("ka", `"${member[i].name} " is a lead singer on " ${song.album}"`); 
-	};
+	},
+	makeSongCard: function () {
+		return 	`<div class="song-card"> <h2></h2> </div>`
+	},
 };
+
+let song1 = Object.create(songMaker);
+song1.title = "Private Idaho";
+song1.artist = "The B-52s";
+console.log("song1", song1);
+
+let song1 = Object.create(songMaker);
+song2.title = "Shiny Happy People";
+song2.artist = "REM";
+console.log("song2", song2 );
+
+
+
+
+
+
+
+
+
+
+
+// const song = {
+// 	title: "Private Idaho",
+// 	artist: "The B-52s",
+// 	plays: 120,
+// 	album: "Wild Planet",
+// 	bandMembers: [
+// 		{name: "Fred", roles: {vocals: "lead", instrument: null} },
+// 		{name: "Cindy", roles: {vocals: "lead", instrument: "keyboard"} },
+// 		{name: "Ricky", roles: {vocals: "background", instrument: "guitar"} }, 
+// 		{name: "Keith", roles: {vocals: "null", instrument: "drums"} },
+// 		{name: "Kate", roles: {vocals: "lead", instrument: null} }
+// 	],
+
+// }
+
+
+// //var bandMembers
+
+// const member = song.bandMembers;
+
+// for (var i = 0; i < member.length; i++) {
+// 	if ("lead" === member[i].roles.vocals) { 
+// 		document.write(`"${member[i].name} " is a lead singer on " ${song.album}"`);
+// 		console.log("blah",`"${member[i].name} " is a lead singer on " ${song.album}"`);
+// 	} else if ("background" === member[i].roles.vocals) {
+// 		document.write(`"${member[i].name}" is a background singer on "${song.album}"`);
+// 		console.log("ha",`"${member[i].name} " is a background singer on " ${song.album}"`);
+// 	} else {
+// 		document.write(`"${member[i].name}" did not sing on "${song.album}"`);
+// 		console.log("ka", `"${member[i].name} " did not sing on " ${song.album}"`); 
+// 	};
+// };
 
 
 // // const vocalRole = member.roles.vocals;
@@ -39,18 +67,6 @@ for (var i = 0; i < member.length; i++) {
 // } else {
 // 	console.log(`${member.name} did not sing on this track`);
 // };
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
